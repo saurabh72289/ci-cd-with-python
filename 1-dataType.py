018dac5d65044d56d36b35483b7630e8de67ce14
saurabh.singh943 <saurabh.singh@antiersollutions.com>
2025-03-20 16:07:39 +0530

    new code

--- 1-dataType.py
+++ 1-dataType.py
@@ -1,37 +1,40 @@
-# # Integer
-# num = 10
-# val = 99
-# # print(num)
+# Integer
+num = 10
+val = 99
+# print(num)
 
-# # Float
-# pi = 3.14
-# # print(pi)
-
-
-# # String
-# name = "Alice"
-# # print(name)
-
-# # Boolean
-# is_active = True
-# # print(is_active)
+# Float
+pi = 3.14
+# print(pi)
 
 
-# # List (mutable)
-# numbers = [1, 2, 3, 4]
-# numbers[1] = 100000
-# # print(numbers[1])
+# String
+name = "Alice"
+# print(name)
 
-# # Tuple (immutable)
-# coordinates = (10, 20, 9990)
-# tupl = (11, 221, 21 )
-# # tupl[0] = 1231231   tuple are immutable
-# print(tupl[0])
-# # print(coordinates[2])
+# Boolean
+is_active = True
+# print(is_active)
 
-# # Dictionary (key-value pair)
-# user = {"name": "Alice", "age": 25}
 
-# # Set (unique values)
-# unique_numbers = {1, 2, 3, 3, 4}
-# print(unique_numbers)
+# List (mutable)
+numbers = [1, 2, 3, 4,"hellooo"]
+numbers[1] = 100000
+print(numbers[4])
+
+# Tuple (immutable)
+coordinates = (10, 20, 9990)
+tupl = (11, 221, 21 )
+# tupl[0] = 1231231   tuple are immutable
+print(tupl[0])
+print(coordinates[0])
+
+# Dictionary (key-value pair)
+user = {"name": "Alice", "age": 25}
+print(user['age'], 'user AGE using dictionary')
+
+# Set (unique values)
+unique_numbers = {1, 2, 3, 3, 4}
+print(unique_numbers)
+
+print(f"the cordinates {coordinates[0]} is situated on the {user['name']} house")
